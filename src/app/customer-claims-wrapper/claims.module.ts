78ef088a8ec629b109031fd1ef5737a14b64e9bb
kartikr <rkartik1992@gmail.com>
2020-03-05 00:53:18 +0530

    remove unnecessary component

--- src/app/customer-claims-wrapper/claims.module.ts
+++ src/app/customer-claims-wrapper/claims.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ClaimsComponent } from './claims.component';
 import { CustomerClaimsModule } from 'customer-claims';
 
 @NgModule({
-  declarations: [ClaimsComponent],
+  declarations: [],
   imports: [CommonModule, CustomerClaimsModule]
 })
 export class ClaimsModule {}
